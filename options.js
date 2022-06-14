360e39bd0401a61d35a493f5ef4b1962de6afa36
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-06-14 14:42:00 +0300

    Add BOM to options page

--- options.js
+++ options.js
@@ -1,20 +1,31 @@
 import {downloadShelf, setDownloadShelf} from "./bg/store/dl-shelf.js";
 import {watchEffect} from "./libs/vue-reactivity.js";
+import {bookmarkOpenerMode, setBookmarkOpenerMode} from "./bg/store/bom.js";
 
 const listUrl = new URL("./list.html", location.href).toString();
 const optionsUrl = new URL("./options.html", location.href).toString();
 document.body.insertAdjacentHTML("afterbegin", `
     <div>List: <a href="${listUrl}">${listUrl}</a></div>
     <div>Options: <a href="${optionsUrl}">${optionsUrl}</a></div>
-    <button id="d" class="btn m-3">Download shelf</button>
+    <button id="download-shelf" class="btn m-3">Download shelf</button>
+    <button id="bom" class="btn m-3">Bookmarks opener mode</button>
 `);
 
-const downloadShelfBtn = document.querySelector("#d");
+const downloadShelfBtn = document.querySelector("#download-shelf");
 downloadShelfBtn.addEventListener("click", async (event) => {
-    console.log(downloadShelf.value);
     await setDownloadShelf(!downloadShelf.value);
 });
 
 watchEffect(() => {
     downloadShelfBtn.textContent = "Download shelf " + (downloadShelf.value ? "✅" : "⬜");
+});
+
+
+const bomBtn = document.querySelector("#bom");
+bomBtn.addEventListener("click", async (event) => {
+    await setBookmarkOpenerMode(!bookmarkOpenerMode.value);
+});
+
+watchEffect(() => {
+    bomBtn.textContent = "Bookmarks opener mode " + (bookmarkOpenerMode.value ? "✅" : "⬜");
 });
