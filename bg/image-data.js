c72047591eca2f411b2c558a49aa7c781aef8286
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-06-16 16:40:53 +0300

    Comment Base85 related code

--- bg/image-data.js
+++ bg/image-data.js
@@ -2,20 +2,20 @@ import {binaryStringToArrayBuffer} from "../util.js";
 
 // In Chrome binary takes more space than base64, however.
 const binary = false;
-
-const useBase85 = false;
-const base85 = {
-    encode: data => data,
-    decode: data => data
-}
+//
+// const useBase85 = false;
+// const base85 = {
+//     encode: data => data,
+//     decode: data => data
+// }
 
 export function toUrl(storeData) {
     let base64 = storeData;
     if (binary) {
         let binaryString = storeData;
-        if (useBase85) {
-            binaryString = base85.decode(storeData);
-        }
+        // if (useBase85) {
+        //     binaryString = base85.decode(storeData);
+        // }
         base64 = btoa(binaryString);
     }
     return "data:image/jpeg;base64," + base64;
@@ -25,9 +25,9 @@ export function toArrayBuffer(storeData) {
     let binaryString;
     if (binary) {
         binaryString = storeData;
-        if (useBase85) {
-            binaryString = base85.decode(storeData);
-        }
+        // if (useBase85) {
+        //     binaryString = base85.decode(storeData);
+        // }
     } else {
         binaryString = atob(storeData);
     }
@@ -38,9 +38,9 @@ export function toStoreData(dataUrl) {
     const base64 = dataUrl.substring("data:image/jpeg;base64,".length);
     if (binary) {
         let binaryString = atob(base64);
-        if (useBase85) {
-            return base85.encode(binaryString);
-        }
+        // if (useBase85) {
+        //     return base85.encode(binaryString);
+        // }
         return binaryString;
     }
     return base64;
