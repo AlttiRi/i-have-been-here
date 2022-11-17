401e462fbfe8de773b50876732883db13767906b
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-11-17 04:42:24 +0300

    Add #list, #json pages

--- opened-tabs.js
+++ opened-tabs.js
@@ -1,13 +1,32 @@
 import {exchangeMessage} from "./util-ext.js";
 
+if (["#list", "#json"].every(hash => hash !== location.hash)) {
+    location.hash = "#list";
+}
+
+const jsonElem = document.querySelector(".json");
+const listElem = document.querySelector(".list");
+
+
+
 void main();
 async function main() {
+    /** @type {chrome.tabs.Tab[]} */
     const tabs = await exchangeMessage("get-tabs--message-exchange");
     console.log(globalThis.tabs = tabs);
 
-    document.body.insertAdjacentHTML("afterbegin", `
+    jsonElem.insertAdjacentHTML("afterbegin", `
         <div>
             <pre>${JSON.stringify(tabs, null, "   ")}</pre>
         </div>        
     `);
+
+    for (const tab of tabs) {
+        listElem.insertAdjacentHTML("beforeend", `
+            <div>
+                <img class="favicon" src="${tab.favIconUrl || "./images/empty.png"}" alt=""/>
+                <a class="url" href="${tab.url}" target="_blank" rel="noreferrer noopener">${tab.url}</a>
+            </div>
+    `.replaceAll(/\s{2,}/g, ""));
+    }
 }
