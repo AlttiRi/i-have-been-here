746a895433a43853eef3eedb22f557dd1b278382
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-11-19 02:42:27 +0300

    Refactor tabs.js

--- pages/tabs/tabs.js
+++ pages/tabs/tabs.js
@@ -1,37 +1,42 @@
 import {exchangeMessage} from "/util-ext.js";
 
 
-
-const btnClearElem  = document.querySelector("#clear-btn");
-const btnAppendElem = document.querySelector("#append-btn");
-const btnSetElem    = document.querySelector("#set-btn");
-const textInputElem = document.querySelector("#text-input");
-
-const jsonElem = document.querySelector("#json-block");
-const listElem = document.querySelector("#list-content");
-
-
-
-btnClearElem?.addEventListener("click", event => {
-    textInputElem.value = "";
-    textInputElem.focus();
-});
-btnSetElem?.addEventListener("click", event => {
-    const urls = parseUrls(textInputElem.value);
-    listElem.innerHTML = "";
-    appendListByUrls(urls);
-});
-btnAppendElem?.addEventListener("click", event => {
-    const urls = parseUrls(textInputElem.value);
-    appendListByUrls(urls);
-});
-
-
 if (location.pathname.endsWith("list.html")) {
     void renderTabList();
 } else
 if (location.pathname.endsWith("json.html")) {
     void renderJson();
+} else
+if (location.pathname.endsWith("input.html")) {
+    handleInputControls();
+}
+
+
+
+
+function handleInputControls() {
+    const textInputElem = document.querySelector("#text-input");
+
+    const btnClearElem  = document.querySelector("#clear-btn");
+    const btnAppendElem = document.querySelector("#append-btn");
+    const btnSetElem    = document.querySelector("#set-btn");
+
+    const listElem = document.querySelector("#list-content");
+
+    btnClearElem.addEventListener("click", event => {
+        textInputElem.value = "";
+        listElem.innerHTML = "";
+        textInputElem.focus();
+    });
+    btnSetElem.addEventListener("click", event => {
+        const urls = parseUrls(textInputElem.value);
+        listElem.innerHTML = "";
+        appendListByUrls(urls, listElem);
+    });
+    btnAppendElem.addEventListener("click", event => {
+        const urls = parseUrls(textInputElem.value);
+        appendListByUrls(urls, listElem);
+    });
 }
 
 
@@ -40,16 +45,15 @@ async function renderTabList() {
     const tabs = await exchangeMessage("get-tabs--message-exchange");
     console.log(globalThis.tabs = tabs);
 
-    listElem.innerHTML = "";
-    appendListByTabs(tabs);
+    const listElem = document.querySelector("#list-content");
+    appendListByTabs(tabs, listElem);
 }
-
 async function renderJson() {
     /** @type {chrome.tabs.Tab[]} */
     const tabs = await exchangeMessage("get-tabs--message-exchange");
     console.log(globalThis.tabs = tabs);
 
-    jsonElem.innerHTML = "";
+    const jsonElem = document.querySelector("#json-block");
     jsonElem.insertAdjacentHTML("afterbegin", `
         <div>
             <pre>${JSON.stringify(tabs, null, "   ")}</pre>
@@ -58,8 +62,34 @@ async function renderJson() {
 }
 
 
-
-
+/** @param {chrome.tabs.Tab[]} tabs
+ *  @param {Element} targetNode */
+function appendListByTabs(tabs, targetNode) {
+    for (const tab of tabs) {
+        targetNode.insertAdjacentHTML("beforeend", `
+            <tr>
+                <td>
+                    <img class="favicon" src="${tab.favIconUrl || "/images/empty.png"}" alt=""/>
+                    <a class="url link-primary" href="${tab.url}" target="_blank" rel="noreferrer noopener">${tab.url}</a>
+                </td>                
+            </tr>
+    `.replaceAll(/\s{2,}/g, ""));
+    }
+}
+/** @param {String[]} urls
+ *  @param {Element} targetNode */
+function appendListByUrls(urls, targetNode) {
+    for (const url of urls) {
+        targetNode.insertAdjacentHTML("beforeend", `
+            <tr>
+                <td>
+                    <img class="favicon" src="/images/empty.png" alt=""/>
+                    <a class="url link-primary" href="${url}" target="_blank" rel="noreferrer noopener">${url}</a>
+                </td>                
+            </tr>
+    `.replaceAll(/\s{2,}/g, ""));
+    }
+}
 
 function parseUrls(urlsText) {
     const urls = urlsText.trim().split(/\s/);
@@ -72,31 +102,4 @@ function parseUrls(urlsText) {
         }
         return url;
     });
-}
-
-/** @param {chrome.tabs.Tab[]} tabs */
-function appendListByTabs(tabs) {
-    for (const tab of tabs) {
-        listElem.insertAdjacentHTML("beforeend", `
-            <tr>
-                <td>
-                    <img class="favicon" src="${tab.favIconUrl || "/images/empty.png"}" alt=""/>
-                    <a class="url link-primary" href="${tab.url}" target="_blank" rel="noreferrer noopener">${tab.url}</a>
-                </td>                
-            </tr>
-    `.replaceAll(/\s{2,}/g, ""));
-    }
-}
-
-function appendListByUrls(urls) {
-    for (const url of urls) {
-        listElem.insertAdjacentHTML("beforeend", `
-            <tr>
-                <td>
-                    <img class="favicon" src="/images/empty.png" alt=""/>
-                    <a class="url link-primary" href="${url}" target="_blank" rel="noreferrer noopener">${url}</a>
-                </td>                
-            </tr>
-    `.replaceAll(/\s{2,}/g, ""));
-    }
 }
