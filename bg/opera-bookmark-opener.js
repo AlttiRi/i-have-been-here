5b21ac5bc1a1e76270f9990803b882cccfc3b201
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-11-20 08:20:37 +0300

    Refactor quick url opener

--- bg/opera-bookmark-opener.js
+++ bg/opera-bookmark-opener.js
@@ -1,5 +1,5 @@
 import {getPopup, getTitle, focusOrCreateNewTab, getActiveTabId} from "../util-ext-bg.js";
-import {watch} from "../libs/vue-reactivity.js";
+import {watch} from "/libs/vue-reactivity.js";
 import {bom, quickAccessUrl} from "./store/store.js";
 
 
@@ -8,7 +8,7 @@ export async function openQuickAccessUrl() {
     return focusOrCreateNewTab(await quickAccessUrl.getValue());
 }
 
-/** @type {{title, popup}} */
+/** @type {{title, popup, saved}} */
 const state = {};
 async function saveState() {
     const tabId = await getActiveTabId();
@@ -19,26 +19,7 @@ function restoreState() {
     const {popup, title} = state;
     chrome.browserAction.setPopup({popup});
     chrome.browserAction.setTitle({title});
-}
-
-async function enable() {
-    await saveState();
-    chrome.browserAction.setPopup({popup: ""});
-    chrome.browserAction.setTitle({title: "Open " + quickAccessUrl.value});
-    chrome.browserAction.onClicked.addListener(openQuickAccessUrl);
-}
-function disable() {
-    restoreState();
-    chrome.browserAction.onClicked.removeListener(openQuickAccessUrl);
-}
-
-
-async function toggle(enabled) {
-    if (enabled) {
-        await enable();
-    } else {
-        disable();
-    }
+    state.saved = false;
 }
 
 export async function enableBookmarksOpenerMode() {
@@ -51,22 +32,28 @@ export async function enableBookmarksOpenerMode() {
         type: "checkbox",
         checked
     });
-    watch(bom.ref, () => {
-        const checked = bom.value;
-        console.log("bookmarkOpenerMode watch", checked);
-        chrome.contextMenus.update(id, {checked});
-    });
     chrome.contextMenus.onClicked.addListener((info, tab) => {
-        if (info.menuItemId === "bookmark_opener") {
+        if (info.menuItemId === id) {
             void bom.setValue(info.checked);
         }
     });
 
-    if (checked) {
-        await enable();
-    }
-    watch(bom.ref, () => {
-        void toggle(bom.value);
+    watch([bom.ref, quickAccessUrl.ref], async () => {
+        if (bom.value) {
+            if (!state.saved) {
+                await saveState();
+                chrome.browserAction.setPopup({popup: ""});
+                chrome.browserAction.onClicked.addListener(openQuickAccessUrl);
+                chrome.contextMenus.update(id, {checked: bom.value});
+                state.saved = true;
+            }
+            chrome.browserAction.setTitle({title: "Open " + quickAccessUrl.value});
+        } else if (state.saved) {
+            restoreState();
+            chrome.browserAction.onClicked.removeListener(openQuickAccessUrl);
+        }
+    }, {
+        immediate: true
     });
 }
 
