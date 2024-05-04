3791a99c8ed7f8039fd51ace8d626950cc291453
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-05-05 00:32:08 +0300

    Use `Controls.vue`

---
+++ src/vue/list/components/Controls.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import {exportVisits, importVisits} from "@/bg/visits";
+import {ScreenshotInfo} from "@/types";
+import {getFromStoreLocal, removeFromStoreLocal} from "@/util-ext";
+import {toArrayBuffer} from "@/bg/image-data";
+import {downloadBlob, fullUrlToFilename, sleep} from "@/util";
+
+
+
+async function importVisitsListener(event: Event) {
+  const inputElement = event.currentTarget as HTMLInputElement;
+  const file: File = inputElement.files![0];
+  console.log(file);
+  const array = JSON.parse(await file.text());
+  await importVisits(array);
+}
+
+async function deleteImages(event: MouseEvent) {
+  const button = event.currentTarget as HTMLButtonElement;
+  button.setAttribute("disabled", "");
+  await removeImages();
+  button.removeAttribute("disabled");
+}
+
+async function removeImages(): Promise<void> {
+  const screenshots: ScreenshotInfo[] = await getFromStoreLocal("screenshots") || [];
+  const promises: Promise<unknown>[] = [];
+  for (const screenshot of screenshots) {
+    promises.push(removeFromStoreLocal(screenshot.scd_id));
+  }
+  await Promise.all(promises);
+}
+
+async function exportImages() {
+  const screenshots: ScreenshotInfo[] = await getFromStoreLocal("screenshots") || [];
+  for (const screenshot of screenshots) {
+    const base64 = await getFromStoreLocal(screenshot.scd_id);
+    const ab = toArrayBuffer(base64);
+    const blob = new Blob([ab], {type: "image/jpeg"});
+    const urlFilename = fullUrlToFilename(screenshot.url);
+    const name = `[ihbh]${urlFilename}.jpg`;
+    downloadBlob(blob, name, screenshot.url);
+    await sleep(125);
+    console.log(name, screenshot.url);
+  }
+}
+</script>
+
+<template>
+  <div class="container" id="controls">
+    <button class="btn btn-outline-primary m-2" id="export-visits"
+            @click="exportVisits"
+    >Export visits
+    </button>
+    <label class="btn btn-outline-primary m-2">
+      <input type="file" accept="application/json" style="display: none"
+             @change="importVisitsListener"
+      >Import visits
+    </label>
+    <button class="btn btn-outline-primary m-2 ms-md-5" id="export-images"
+            @click="exportImages"
+    >Export images</button>
+    <button class="btn btn-outline-danger m-2" id="delete-images"
+            @click="deleteImages"
+    >Delete images</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/vue/list/components/List.vue
+++ src/vue/list/components/List.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import {onMounted, ref} from "vue";
-import {toArrayBuffer}         from "@/bg/image-data";
 import {ScreenshotInfo, Visit} from "@/types";
-import {exportVisits, getVisits, importVisits}   from "@/bg/visits";
-import {getFromStoreLocal, removeFromStoreLocal} from "@/util-ext";
-import {downloadBlob, fullUrlToFilename, sleep}  from "@/util";
+import {getVisits}         from "@/bg/visits";
+import {getFromStoreLocal} from "@/util-ext";
+import {sleep}             from "@/util";
 
 import Header         from "./Header.vue";
 import VisitItem      from "./VisitItem.vue";
 import ScreenshotItem from "./ScreenshotItem.vue";
+import Controls       from "./Controls.vue";
 
 
 const visits = ref<Visit[]>([]);
@@ -17,52 +17,12 @@ getVisits().then(value => {
   console.log("Visits:", value);
 });
 
-
 const screenshots = ref<ScreenshotInfo[]>([]);
 getFromStoreLocal("screenshots").then(value => {
   screenshots.value = value || [];
   console.log("Screenshots:", value);
 });
 
-
-
-async function importVisitsListener(event: Event) {
-  const inputElement = event.currentTarget as HTMLInputElement;
-  const file: File = inputElement.files![0];
-  console.log(file);
-  const array = JSON.parse(await file.text());
-  await importVisits(array);
-}
-
-async function deleteImages(event: MouseEvent) {
-  const button = event.currentTarget as HTMLButtonElement;
-  button.setAttribute("disabled", "");
-  await removeImages();
-  button.removeAttribute("disabled");
-}
-
-async function removeImages(): Promise<void> {
-  const screenshots: ScreenshotInfo[] = await getFromStoreLocal("screenshots") || [];
-  const promises: Promise<unknown>[] = [];
-  for (const screenshot of screenshots) {
-    promises.push(removeFromStoreLocal(screenshot.scd_id));
-  }
-  await Promise.all(promises);
-}
-
-async function exportImages() {
-  const screenshots: ScreenshotInfo[] = await getFromStoreLocal("screenshots") || [];
-  for (const screenshot of screenshots) {
-    const base64 = await getFromStoreLocal(screenshot.scd_id);
-    const ab = toArrayBuffer(base64);
-    const blob = new Blob([ab], {type: "image/jpeg"});
-    const urlFilename = fullUrlToFilename(screenshot.url);
-    const name = `[ihbh]${urlFilename}.jpg`;
-    downloadBlob(blob, name, screenshot.url);
-    await sleep(125);
-    console.log(name, screenshot.url);
-  }
-}
 
 onMounted(async function scrollToHash() {
   const hash = location.hash;
@@ -77,30 +37,13 @@ onMounted(async function scrollToHash() {
   location.hash = "";
   location.hash = hash;
 });
-
 </script>
 
 <template>
   <div data-comp="List" id="list">
     <Header/>
     <hr>
-    <div class="container" id="controls">
-      <button class="btn btn-outline-primary m-2" id="export-visits"
-              @click="exportVisits"
-      >Export visits
-      </button>
-      <label class="btn btn-outline-primary m-2">
-        <input type="file" accept="application/json" style="display: none"
-               @change="importVisitsListener"
-        >Import visits
-      </label>
-      <button class="btn btn-outline-primary m-2 ms-md-5" id="export-images"
-              @click="exportImages"
-      >Export images</button>
-      <button class="btn btn-outline-danger m-2" id="delete-images"
-              @click="deleteImages"
-      >Delete images</button>
-    </div>
+    <Controls/>
     <hr>
     <div id="visits">
       <VisitItem v-for="visit of visits" :visit/>
