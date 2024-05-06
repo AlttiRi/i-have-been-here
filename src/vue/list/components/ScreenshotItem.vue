d0e77529be6b41a3c9f22b077af7cdb9c96fc86c
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-05-06 21:47:07 +0300

    Update `ScreenshotItem`

--- src/vue/list/components/ScreenshotItem.vue
+++ src/vue/list/components/ScreenshotItem.vue
@@ -16,14 +16,25 @@ getFromStoreLocal(scd_id).then(base64 => src.value = toJpgDataUrl(base64));
 </script>
 
 <template>
-  <div data-comp="ScreenshotItem" class="screenshot-info" :data-hash="scd_id" :data-created="created">
-    <h3 class="title" style="align-self: start; margin: 12px;">{{ title || fullUrlToFilename(url) }}</h3>
-    <div class="url"  style="align-self: start; margin: 12px;">{{ url }}</div>
-    <div class="created" style="align-self: start; margin: 12px;">{{ created ? dateFormatter(created) : "" }}</div>
+  <div data-comp="ScreenshotItem" class="screenshot-item" :data-hash="scd_id" :data-created="created">
+    <div class="info">
+      <h3 class="title" style="align-self: start; margin: 12px;">{{ title || fullUrlToFilename(url) }}</h3>
+      <div class="url"  style="align-self: start; margin: 12px;">{{ url }}</div>
+      <div class="created" style="align-self: start; margin: 12px;">{{ created ? dateFormatter(created) : "" }}</div>
+    </div>
     <img :src="src" :alt="title" :title="fullUrlToFilename(url)">
   </div>
 </template>
 
 <style scoped>
-
+.info {
+  padding-left: 12px;
+}
+.screenshot-item:hover .info {
+  padding-left: 11px;
+  border-left: 1px #0d6efd solid;
+}
+a:visited {
+  color: #0a53be;
+}
 </style>
