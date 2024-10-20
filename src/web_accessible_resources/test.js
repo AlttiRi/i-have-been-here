a57708a582568dc3618978c803b6a5a22c4e32ad
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-10-21 02:48:46 +0300

    Update `web_accessible_resources` demo

--- src/web_accessible_resources/test.js
+++ src/web_accessible_resources/test.js
@@ -1,4 +1,4 @@
-console.log("from web_accessible_resources/test.js");
+console.log("👾", "from .../web_accessible_resources/test.js");
 /*
 // to test it //
 
@@ -6,15 +6,12 @@ document.querySelector("body")
     .append((function(){
         const s = document.createElement("script");
         s.type = "module";
-        s.src = `chrome-extension://gjcpkeofcjdjkhafgomjpcpoplngijlp/web_accessible_resources/test.js`;
+        s.src = `chrome-extension://ljjoafkhpecgfpppmbkenhojmcilpmgp/src/web_accessible_resources/test.js`;
         return s;
     })());
 
 
 //     "web_accessible_resources": [
 //         "src/web_accessible_resources/*",
-//         "src/content.js",
-//         "src/util-ext.js",
-//         "src/util.js"
 //     ],
 */
