137a57a42177de317ceaa3875210370b44f80c6c
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-11-02 03:33:12 +0300

    Add test external message logging

--- src/bg/inits/message-logger.ts
+++ src/bg/inits/message-logger.ts
@@ -1,8 +1,14 @@
 import {inIncognitoContext} from "@/utils/util-ext";
+import {logOrange} from "@/utils/util";
 
 
 export function initLogEverything(): void {
     chrome.runtime.onMessage.addListener(function logEverything(message: any, sender: chrome.runtime.MessageSender) {
         console.log(`[${inIncognitoContext ? "⬛" : "⬜"}][Incoming message]`, message, {from: sender});
+    });
+
+    // chrome.runtime.sendMessage("ljjoafkhpecgfpppmbkenhojmcilpmgp", `message from ${chrome.runtime.id}`);
+    chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
+        logOrange("[External message]", {request, sender, sendResponse})();
     });
 }
